9a938a81f1ff57e3e48329b1f3bbe702437222f5
Strrum <97414600+vluuzi@users.noreply.github.com>
2024-12-13 19:29:01 -0500

    Update vaft-ublock-origin.js
    edited text box

--- vaft/vaft-ublock-origin.js
+++ vaft/vaft-ublock-origin.js
@@ -150,7 +150,7 @@ twitch-videoad.js text/javascript
                         if (adBlockDiv == null) {
                             adBlockDiv = getAdBlockDiv();
                         }
-                        adBlockDiv.P.textContent = 'Blocking ads';
+                        adBlockDiv.P.textContent = ' ';
                         adBlockDiv.style.display = 'block';
                     } else if (e.data.key == 'HideAdBlockBanner') {
                         if (adBlockDiv == null) {
